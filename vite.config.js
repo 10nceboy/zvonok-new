1c05ebab8af7478084d135c0429c702b7d2170b1
10nceboy <10nceboy@gmail.com>
2023-09-14 23:38:58 +0300

    confirmation page edit

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,38 @@
-import { resolve } from "path";
-import { defineConfig } from "vite";
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: "/",
+  base: '/',
   server: {
-    port: 3004,
+    port: 3004
   },
   resolve: {
     alias: {
-      "~": resolve(__dirname, "js"),
+      '~': resolve(__dirname, 'js')
     },
-    extensions: [".js", ".css"],
+    extensions: ['.js', '.css']
   },
 
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"),
-        api: resolve(__dirname, "api-call.html"),
-        boring: resolve(__dirname, "boring-page.html"),
-        callcenter: resolve(__dirname, "callcenter.html"),
-        claim: resolve(__dirname, "claim.html"),
-        confirmation: resolve(__dirname, "confirmation.html"),
-        autocall: resolve(__dirname, "auto-call.html"),
-        faq: resolve(__dirname, "faq.html"),
-        contacts: resolve(__dirname, "contacts.html"),
-        nonboring: resolve(__dirname, "non-boring-page.html"),
-        links: resolve(__dirname, "links.html"),
-        manual: resolve(__dirname, "manual.html"),
-        incoming: resolve(__dirname, "incoming.html"),
-        seo: resolve(__dirname, "seo-page.html"),
-        price: resolve(__dirname, "price.html"),
-        videoinstruction: resolve(__dirname, "videoinstruction.html"),
-      },
-    },
-  },
+        main: resolve(__dirname, 'index.html'),
+        api: resolve(__dirname, 'api-call.html'),
+        boring: resolve(__dirname, 'boring-page.html'),
+        callcenter: resolve(__dirname, 'callcenter.html'),
+        claim: resolve(__dirname, 'claim.html'),
+        confirmation: resolve(__dirname, 'confirmation.html'),
+        autocall: resolve(__dirname, 'auto-call.html'),
+        faq: resolve(__dirname, 'faq.html'),
+        contacts: resolve(__dirname, 'contacts.html'),
+        nonboring: resolve(__dirname, 'non-boring-page.html'),
+        links: resolve(__dirname, 'links.html'),
+        manual: resolve(__dirname, 'manual.html'),
+        incoming: resolve(__dirname, 'incoming.html'),
+        seo: resolve(__dirname, 'seo-page.html'),
+        price: resolve(__dirname, 'price.html'),
+        videoinstruction: resolve(__dirname, 'videoinstruction.html')
+      }
+    }
+  }
 });
